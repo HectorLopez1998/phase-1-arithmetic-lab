88680bc601f49648c425dd6ed5bed830f3810923
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-07-14 22:10:51 -0400

    commit tree 6f98fcb8e02eb5a8a6f391e5ae0a3468e8697fda

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 // Write your code here
+
+const random = Math.ceil(Math.random());
+const num3 = 16
+const num4 = 4
+const mod = num3 / num4;
+
+const max = 20
